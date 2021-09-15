2831f93da8acb046efcd13286cd02a30ba6896fa
ahmadnadhifmuhajir <ahmadnadhifmuhajir@gmail.com>
2021-09-14 18:11:11 -1100

    First Commit

---
+++ Assets/Interaction/Scripts/Interaction_trs.cs
@@ -0,0 +1,33 @@
+﻿/**
+ * @author Ahmad Nadhif Muhajir (Teeres)
+ * @version 1.0
+ */
+ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Interaction_trs : MonoBehaviour
+{
+    
+    public void OnOBJ (GameObject xobj) {
+        xobj.SetActive(true);
+    }
+
+    public void OffOBJ (GameObject xobj) {
+        xobj.SetActive(false);
+    }
+
+    public void GotoLevelWithText (Text xtext) {
+        string xlevel = xtext.text;
+        Debug.Log (xlevel);
+        SceneManager.LoadScene (xlevel);
+    }
+
+    public void GotoLevelWithString (string xlevel) {
+        SceneManager.LoadScene (xlevel);
+    }
+   
+}
---
+++ Assets/Interaction/Scripts/TextStringConvert_trs.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * @author Ahmad Nadhif Muhajir (Teeres)
+ * @version 1.0
+ */
+ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TextStringConvert_trs : MonoBehaviour
+{
+    public string trs_string;
+    public Text trs_text;
+    
+    public void TextToString (Text xtext) {
+        trs_string = xtext.text;
+    }
+    
+    public void StringToText (string xstring) {
+        trs_text.text = trs_string.ToString();
+    }
+}
